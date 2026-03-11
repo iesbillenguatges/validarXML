7dfb638d1f0cbb441e06ddaf6a879b3db5643184
iesbillenguatges <150657682+iesbillenguatges@users.noreply.github.com>
2026-03-11 12:30:43 +0100

    Enhance XML validation with error handling
    Added error handling for XML validation warnings and errors.

--- validarXML.java
+++ validarXML.java
@@ -1,19 +1,39 @@
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.SAXException;
 import java.io.File;
 
 public class ValidarXML {
     public static void main(String[] args) {
         try {
+
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setValidating(true);
 
             DocumentBuilder builder = factory.newDocumentBuilder();
+
+            builder.setErrorHandler(new ErrorHandler() {
+                public void warning(SAXParseException e) {
+                    System.out.println("Warning: " + e.getMessage());
+                }
+
+                public void error(SAXParseException e) throws SAXException {
+                    throw e;
+                }
+
+                public void fatalError(SAXParseException e) throws SAXException {
+                    throw e;
+                }
+            });
+
             builder.parse(new File("cataleg.xml"));
 
             System.out.println("XML valid!");
+
         } catch (Exception e) {
-            System.out.println("XML no valid");
+            System.out.println("XML no valid: " + e.getMessage());
         }
     }
 }
